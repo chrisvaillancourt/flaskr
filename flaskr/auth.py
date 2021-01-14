3eb55789087591d8e20a43403a83eb2d790aedd4
Chris Vaillancourt <cvaillancourt@denovium.com>
2021-01-13 17:20:09 -0800

    feat(auth.py): add register view

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -8,3 +8,42 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+# @bp.route associates the /register url with the register [view] fn
+# When flask receives a request to /auth/register, it calls the register fn and use the returned value as the response
+
+# If a user submits the form, request.method == 'POST' and will start
+# validating the input.
+
+# request.form is a special type of dict mapping submitted form keys and values.
+# see https://docs.python.org/3/library/stdtypes.html#dict
+
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
+
+        flash(error)
+
+    return render_template('auth/register.html')
